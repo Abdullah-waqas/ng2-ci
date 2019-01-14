72a3e64ba7d73bf22192951cb315685a573a2016
Abdullah <dev.waqas08@gmail.com>
2019-01-14 23:19:29 +0500

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,110 @@
-import { TestBed, async } from '@angular/core/testing';
+import { HeaderComponent } from './header/header.component';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let debugElement: DebugElement;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FormsModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        FooterComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    debugElement = fixture.debugElement;
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'ng2-ci'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('ng2-ci');
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng2-ci!');
   });
+
+  it('should set value in input', () => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    console.log(compiled.querySelector('input').value);
+    // expect(compiled.querySelector('input').value).toContain('title');
+  });
+
+  it('should keep input and h2 in sync', () => {
+    fixture.detectChanges();
+
+    const inputDe = fixture.debugElement.query(By.css('input[name="title"]'));
+    const inputEl = inputDe.nativeElement;
+    inputEl.value = 'Updated Task 12';
+    inputEl.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    const de = fixture.debugElement.query(By.css('h2'));
+    // expect(de.nativeElement.textContent).toEqual('Updated Task 1');
+  });
+
+  it('should increment/decrement value', () => {
+    fixture.componentInstance.increment();
+    expect(fixture.componentInstance.value).toEqual(1);
+
+    fixture.componentInstance.decrement();
+    expect(fixture.componentInstance.value).toEqual(0);
+  });
+
+  it('should increment in template', () => {
+    // fixture.componentInstance.value = 0;
+    debugElement
+      .query(By.css('button.increment'))
+      .triggerEventHandler('click', null);
+
+    fixture.detectChanges();
+    const value = debugElement.query(By.css('p')).nativeElement.innerText;
+    console.log(value);
+    // expect(value).toEqual('1');
+  });
+
+  it('should stop at 0 and show minimum message', () => {
+    debugElement
+      .query(By.css('button.decrement'))
+      .triggerEventHandler('click', null);
+
+    fixture.detectChanges();
+    const message = debugElement.query(By.css('p.message')).nativeElement.innerText;
+
+    expect(fixture.componentInstance.value).toEqual(0);
+    expect(message).toContain('Minimum');
+  });
+
+  it('should stop at 15 and show maximum message', () => {
+    fixture.componentInstance.value = 15;
+    debugElement
+      .query(By.css('button.increment'))
+      .triggerEventHandler('click', null);
+
+    fixture.detectChanges();
+    const message = debugElement.query(By.css('p.message')).nativeElement.innerText;
+
+    expect(fixture.componentInstance.value).toEqual(15);
+    expect(message).toContain('Maximum1');
+  });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+const K = 234;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng2-ci';
+  value = 0;
+  message: string;
+  abcConst = K;
+
+  increment() {
+    if (this.value < 15) {
+      this.value += 1;
+      this.message = '';
+    } else {
+      this.message = 'Maximum reached!';
+    }
+    console.log(this.message);
+  }
+  decrement() {
+    if (this.value > 0) {
+      this.value -= 1;
+      this.message = '';
+    } else {
+      this.message = 'Minimum reached!';
+    }
+  }
+
 }
